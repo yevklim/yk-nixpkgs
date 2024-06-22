a8e3069c17ccc51bb861dde8c9f83eb006ca4247
Yevhenii <64846678+yevklim@users.noreply.github.com>
2024-06-22 17:04:00 +0300

    added default.nix to allow impure evaluations

--- flake.nix
+++ flake.nix
@@ -11,20 +11,5 @@
       system = "x86_64-linux";
       config.allowUnfree = true;
     };
-    yevklim = {
-      name = "yevklim";
-      email = "64846678+yevklim@users.noreply.github.com";
-      github = "yevklim";
-      githubId = 64846678;
-    };
-    maintainers = {
-      inherit yevklim;
-    };
-    callPackage = pkgs.lib.callPackageWith (pkgs // packages // maintainers);
-    packages = {
-      newaita-reborn-icon-theme = callPackage ./packages/newaita-reborn-icon-theme { };
-      papirus-newaita-icon-theme = callPackage ./packages/papirus-newaita-icon-theme { };
-      viber = callPackage ./packages/viber { };
-    };
-  in packages;
+  in import ./. { inherit pkgs; };
 }
