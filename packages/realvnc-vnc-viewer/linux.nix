365aec998bc90bba277172cd25362adebd01ded9
Yevhenii <64846678+yevklim@users.noreply.github.com>
2024-06-29 14:29:33 +0300

    yk-pkgs/realvnc-vnc-viewer: support aarch64-linux

--- packages/realvnc-vnc-viewer/linux.nix
+++ packages/realvnc-vnc-viewer/linux.nix
@@ -2,22 +2,24 @@
 , stdenv
 , requireFile
 , autoPatchelfHook
-, rpmextract
+, dpkg
 , libX11
 , libXext
 , pname
 , version
 , meta
+, makeDesktopItem
+, copyDesktopItems
 }:
 
 stdenv.mkDerivation (finalAttrs: {
   inherit pname version;
 
-  src = {
-    "x86_64-linux" = requireFile rec {
-      name = "VNC-Viewer-${finalAttrs.version}-Linux-x64.rpm";
+  src = let
+    mkSource = { arch, sha256 }: requireFile rec {
+      name = "VNC-Viewer-${finalAttrs.version}-Linux-${arch}.deb";
       url = "https://downloads.realvnc.com/download/file/viewer.files/${name}";
-      sha256 = "sha256-KJZbH3mfxuyUslkYvB/RKquEsB7ayJSv6yNqfLmAsGI=";
+      inherit sha256;
       message= ''
         vnc-viewer can be downloaded from ${url},
         but the download link require captcha, thus if you wish to use this application,
@@ -26,13 +28,32 @@ stdenv.mkDerivation (finalAttrs: {
         $ nix-prefetch-url --type sha256 file:///path/to/${name}
       '';
     };
+  in {
+    "x86_64-linux" = mkSource {
+      arch = "x64";
+      sha256 = "sha256-ptBA2jWmjw1q6kvBxwoAv/R/7l8GxfeMPifnGm6+mvg=";
+    };
+    "aarch64-linux" = mkSource { # rpm is not available for aarch64
+      arch = "ARM64";
+      sha256 = "sha256-WoeAE1/lLUbkdA91TE9Qu3VEn4xcsd75o+442Mrizcs=";
+    };
   }.${stdenv.system} or (throw "Unsupported system: ${stdenv.hostPlatform.system}");
 
-  nativeBuildInputs = [ autoPatchelfHook rpmextract ];
+  nativeBuildInputs = [ autoPatchelfHook dpkg copyDesktopItems ];
   buildInputs = [ libX11 libXext stdenv.cc.cc.libgcc or null ];
 
+  desktopItems = [
+    (makeDesktopItem { # included in the rpm but not the deb package
+      name = "realvnc-vncviewer-scheme";
+      desktopName = "VNC Viewer URL Handler";
+      exec = "vncviewer -uri %u";
+      mimeTypes = [ "x-scheme-handler/com.realvnc.vncviewer.connect" ];
+      noDisplay = true;
+    })
+  ];
+
   unpackPhase = ''
-    rpmextract $src
+    dpkg-deb -x $src .
   '';
 
   postPatch = ''
